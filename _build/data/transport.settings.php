72329c6275178fc47eb326271d149e8a640c6751
Володя Володин <vovanblya@mail.ru>
2015-12-04 14:35:22 +0400

    -

--- _build/data/transport.settings.php
+++ _build/data/transport.settings.php
@@ -7,21 +7,21 @@ $tmp = array(
 
 	//временные
 
-	'assets_path' => array(
-		'value' => '{base_path}skypenotify/assets/components/skypenotify/',
-		'xtype' => 'textfield',
-		'area' => 'skypenotify_temp',
-	),
-	'assets_url' => array(
-		'value' => '/skypenotify/assets/components/skypenotify/',
-		'xtype' => 'textfield',
-		'area' => 'skypenotify_temp',
-	),
-	'core_path' => array(
-		'value' => '{base_path}skypenotify/core/components/skypenotify/',
-		'xtype' => 'textfield',
-		'area' => 'skypenotify_temp',
-	),
+//	'assets_path' => array(
+//		'value' => '{base_path}skypenotify/assets/components/skypenotify/',
+//		'xtype' => 'textfield',
+//		'area' => 'skypenotify_temp',
+//	),
+//	'assets_url' => array(
+//		'value' => '/skypenotify/assets/components/skypenotify/',
+//		'xtype' => 'textfield',
+//		'area' => 'skypenotify_temp',
+//	),
+//	'core_path' => array(
+//		'value' => '{base_path}skypenotify/core/components/skypenotify/',
+//		'xtype' => 'textfield',
+//		'area' => 'skypenotify_temp',
+//	),
 
 	//временные
 
